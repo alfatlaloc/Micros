40a49615679ec4ff2accf4a90c90f79b6e765612
alfatlaloc <mr.alfatlaloc@gmail.com>
2020-05-31 16:10:11 -0500

    arduinaso

---
+++ arduinaso/sketch_may20a/sketch_may20a.ino
@@ -0,0 +1,16 @@
+void setup() {
+  pinMode(13, OUTPUT);    // sets the digital pin 13 as output
+}
+
+void loop() {
+  digitalWrite(13, HIGH); // sets the digital pin 13 on
+  digitalWrite(13, HIGH); // sets the digital pin 13 on
+  digitalWrite(13, HIGH); // sets the digital pin 13 on
+  digitalWrite(13, HIGH); // sets the digital pin 13 on
+  digitalWrite(13, HIGH); // sets the digital pin 13 on
+  digitalWrite(13, LOW);  // sets the digital pin 13 off
+  digitalWrite(13, LOW);  // sets the digital pin 13 off
+  digitalWrite(13, LOW);  // sets the digital pin 13 off
+  digitalWrite(13, LOW);  // sets the digital pin 13 off
+  digitalWrite(13, LOW);  // sets the digital pin 13 off
+}
